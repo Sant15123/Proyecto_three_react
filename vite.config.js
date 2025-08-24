4dd2d3c5dd357e02612129335521b534c2875cb8
Sant15123 <sansuadur2006@gmail.com>
2025-08-23 20:04:42 -0500

    Agregué nuevos archivos al proyecto

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Proyecto_three_react/', // Necesario para GitHub Pages
   plugins: [react()],
 })
