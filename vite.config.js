369fee4c9537ddf9035fdc1a7e8389c34773e4eb
Sant15123 <sansuadur2006@gmail.com>
2025-08-23 21:03:21 -0500

    S

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Proyecto_three_react/', // 👈 importante, coincide con el repo
+  base: '/Proyecto_three_react/',
 })
 
 
