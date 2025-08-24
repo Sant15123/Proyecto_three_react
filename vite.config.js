306631a90b59003357f590dd1fa9c7d5fe936923
Sant15123 <sansuadur2006@gmail.com>
2025-08-23 20:47:53 -0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/Proyecto_three_react/', // Necesario para GitHub Pages
+  plugins: [react()],
+  base: '/Proyecto_three_react/', // 👈 importante, coincide con el repo
 })
 
 
